4003a9efad0fa893d579949c6c8d0a43c101233b
Leonard Maisha <me@example.com>
2025-07-30 15:01:15 +0300

    adds changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,43 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleDeleteTask = (taskToDelete) => {
+    const updatedTasks = tasks.filter(task => task !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter(task => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleAddTask}
+      />
+      <TaskList
+        tasks={filteredTasks}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
